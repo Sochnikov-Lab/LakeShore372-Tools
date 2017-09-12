0179feea66ac11d2066c2ee874ce3171c0c82b0a
SochnikovLab <sochnikovlab@gmail.com>
2017-09-12 14:44:51 -0400

    joe: fixed some issues with new order, added progbars

--- SimplifiedStressMeasure.py
+++ SimplifiedStressMeasure.py
@@ -8,6 +8,18 @@ from datetime import datetime
 from configparser import ConfigParser
 import LakeShore372 as ls
 
+def sleepprogbar(timeToSleep):
+    i = 0
+    while i <= timeToSleep:
+        prog = ((100 * i) / timeToSleep) + 0.0
+        timeremaining = timeToSleep - i
+        stringtoprint = "    -> Sleep progress: /" + int(prog/5)*"/" + (20 - int(prog/5))*" " + "/ " + str(int(timeremaining)) + " seconds left.      \r"
+        print stringtoprint,
+        #print ""
+        i += 1
+        sleep(1)
+    print "    -> Sleep progress: ////////////////////// DONE                    \r",
+    print ""
 
 print("===Debug===")
 ###Script###
@@ -61,7 +73,7 @@ while currentpc > LSHDev.sampleheater["finalpc"]:
     print("Heater is confirmed to be set to:" + ht_check)
     #Wait Thermalization
     print("Sleeping for Thermalization: " + str(LSHDev.timeConstants["t_therm"]) + " seconds")
-    sleep(LSHDev.timeConstants["t_therm"]+t_safety)
+    sleepprogbar(LSHDev.timeConstants["t_therm"])
     print("Thermalization Over")
     for measurement in range(0,LSHDev.scanner["scannerpasses"]):
 
@@ -70,7 +82,7 @@ while currentpc > LSHDev.sampleheater["finalpc"]:
         sleep(1)
         LSHDev.ScanTo(LSHDev.mcthermo)
         print("o  Sleeping for switch/filter settle/0.5*dwell: " + str(LSHDev.timeConstants["t_switch"] + LSHDev.mcthermo["t_settle"] + LSHDev.mcthermo["t_dwell"]) + " seconds")
-        sleep(LSHDev.timeConstants["t_switch"] + LSHDev.mcthermo["t_settle"]+0.5*LSHDev.mcthermo["t_dwell"])
+        sleepprogbar(LSHDev.timeConstants["t_switch"] + LSHDev.mcthermo["t_settle"]+0.5*LSHDev.mcthermo["t_dwell"])
         TempProbeR = LSHDev.ReadResistance(LSHDev.mcthermo)
         print("o  Read Resistance: " + str(TempProbeR) +" Ohms")
         sleep(0.25)
@@ -80,20 +92,20 @@ while currentpc > LSHDev.sampleheater["finalpc"]:
         LSHData.AppendMCThermoR(TempProbeR)
         print("o  Appended Data to arrays")
         print("o  Sleeping for 0.5*dwell: " + str(0.5*LSHDev.mcthermo["t_dwell"]) + " seconds")
-        sleep(0.5*LSHDev.mcthermo["t_dwell"])
+        sleepprogbar(0.5*LSHDev.mcthermo["t_dwell"])
 
         #Sample 1
         print(":Scanner: Sample1:")
         sleep(1)
         LSHDev.ScanTo(LSHDev.sample1)
         print("o  Sleeping for switch/filter settle/0.5*dwell: " + str(LSHDev.timeConstants["t_switch"] + LSHDev.sample1["t_settle"] + 0.5*LSHDev.sample1["t_dwell"]) + " seconds")
-        sleep(LSHDev.timeConstants["t_switch"] + LSHDev.sample1["t_settle"]+0.5*LSHDev.sample1["t_dwell"])
+        sleepprogbar(LSHDev.timeConstants["t_switch"] + LSHDev.sample1["t_settle"]+0.5*LSHDev.sample1["t_dwell"])
         Sample1R = LSHDev.ReadResistance(LSHDev.sample1)
         print("o  Read Resistance: " + str(Sample1R) + " Ohms")
         LSHData.AppendSample1R(Sample1R)
         print("o  Appended Data to arrays")
         print("o  Sleeping for 0.5*dwell: " + str(0.5*LSHDev.sample1["t_dwell"]) + " seconds")
-        sleep(0.5*LSHDev.sample1["t_dwell"])
+        sleepprogbar(0.5*LSHDev.sample1["t_dwell"])
         LSHData.UpdateCSV_S1(currentpc)
 
         #MC Thermometer
@@ -101,7 +113,7 @@ while currentpc > LSHDev.sampleheater["finalpc"]:
         sleep(1)
         LSHDev.ScanTo(LSHDev.mcthermo)
         print("o  Sleeping for switch/filter settle/0.5*dwell: " + str(LSHDev.timeConstants["t_switch"] + LSHDev.mcthermo["t_settle"] + LSHDev.mcthermo["t_dwell"]) + " seconds")
-        sleep(LSHDev.timeConstants["t_switch"] + LSHDev.mcthermo["t_settle"]+0.5*LSHDev.mcthermo["t_dwell"])
+        sleepprogbar(LSHDev.timeConstants["t_switch"] + LSHDev.mcthermo["t_settle"]+0.5*LSHDev.mcthermo["t_dwell"])
         TempProbeR = LSHDev.ReadResistance(LSHDev.mcthermo)
         print("o  Read Resistance: " + str(TempProbeR) +" Ohms")
         sleep(0.25)
@@ -111,21 +123,20 @@ while currentpc > LSHDev.sampleheater["finalpc"]:
         LSHData.AppendMCThermoR(TempProbeR)
         print("o  Appended Data to arrays")
         print("o  Sleeping for 0.5*dwell: " + str(0.5*LSHDev.mcthermo["t_dwell"]) + " seconds")
-        sleep(0.5*LSHDev.mcthermo["t_dwell"])
-        LSHData.UpdateCSV_S1(currentpc)
+        sleepprogbar(0.5*LSHDev.mcthermo["t_dwell"])
 
         #Sample 2
         print(":Scanner: Sample2:")
         sleep(1)
         LSHDev.ScanTo(LSHDev.sample2)
         print("o  Sleeping for switch/filter settle/0.5*dwell: " + str(LSHDev.timeConstants["t_switch"] + LSHDev.sample2["t_settle"] + 0.5*LSHDev.sample2["t_dwell"]) + " seconds")
-        sleep(LSHDev.timeConstants["t_switch"] + LSHDev.sample2["t_settle"]+0.5*LSHDev.sample2["t_dwell"])
+        sleepprogbar(LSHDev.timeConstants["t_switch"] + LSHDev.sample2["t_settle"]+0.5*LSHDev.sample2["t_dwell"])
         Sample2R = LSHDev.ReadResistance(LSHDev.sample2)
         print("o  Read Resistance: " + str(Sample2R) + " Ohms")
         LSHData.AppendSample2R(Sample2R)
         print("o  Appended Data to arrays")
         print("o  Sleeping for 0.5*dwell: " + str(0.5*LSHDev.sample2["t_dwell"]) + " seconds")
-        sleep(0.5*LSHDev.sample2["t_dwell"])
+        sleepprogbar(0.5*LSHDev.sample2["t_dwell"])
         LSHData.UpdateCSV_S2(currentpc)
 
         #MC Thermometer
@@ -133,7 +144,7 @@ while currentpc > LSHDev.sampleheater["finalpc"]:
         sleep(1)
         LSHDev.ScanTo(LSHDev.mcthermo)
         print("o  Sleeping for switch/filter settle/0.5*dwell: " + str(LSHDev.timeConstants["t_switch"] + LSHDev.mcthermo["t_settle"] + LSHDev.mcthermo["t_dwell"]) + " seconds")
-        sleep(LSHDev.timeConstants["t_switch"] + LSHDev.mcthermo["t_settle"]+0.5*LSHDev.mcthermo["t_dwell"])
+        sleepprogbar(LSHDev.timeConstants["t_switch"] + LSHDev.mcthermo["t_settle"]+0.5*LSHDev.mcthermo["t_dwell"])
         TempProbeR = LSHDev.ReadResistance(LSHDev.mcthermo)
         print("o  Read Resistance: " + str(TempProbeR) +" Ohms")
         sleep(0.25)
@@ -143,25 +154,24 @@ while currentpc > LSHDev.sampleheater["finalpc"]:
         LSHData.AppendMCThermoR(TempProbeR)
         print("o  Appended Data to arrays")
         print("o  Sleeping for 0.5*dwell: " + str(0.5*LSHDev.mcthermo["t_dwell"]) + " seconds")
-        sleep(0.5*LSHDev.mcthermo["t_dwell"])
-        LSHData.UpdateCSV_S1(currentpc)
+        sleepprogbar(0.5*LSHDev.mcthermo["t_dwell"])
 
         #Sample 3
         print(":Scanner: Sample3:")
         sleep(1)
         LSHDev.ScanTo(LSHDev.sample3)
         print("o  Sleeping for switch/filter settle/0.5*dwell: " + str(LSHDev.timeConstants["t_switch"] + LSHDev.sample3["t_settle"] + 0.5*LSHDev.sample3["t_dwell"]) + " seconds")
-        sleep(LSHDev.timeConstants["t_switch"] + LSHDev.sample3["t_settle"]+0.5*LSHDev.sample3["t_dwell"])
+        sleepprogbar(LSHDev.timeConstants["t_switch"] + LSHDev.sample3["t_settle"]+0.5*LSHDev.sample3["t_dwell"])
         Sample3R = LSHDev.ReadResistance(LSHDev.sample3)
         print("o  Read Resistance: " + str(Sample3R) + " Ohms")
         LSHData.AppendSample3R(Sample3R)
         print("o  Appended Data to arrays")
         print("o  Sleeping for 0.5*dwell: " + str(0.5*LSHDev.sample2["t_dwell"]) + " seconds")
-        sleep(0.5*LSHDev.sample2["t_dwell"])
+        sleepprogbar(0.5*LSHDev.sample2["t_dwell"])
         LSHData.UpdateCSV_S3(currentpc)
 
         #Update CSV
-        LSHData.UpdateCSV(currentpc)
+        #LSHData.UpdateCSV(currentpc)
         #LSHData.UpdatePlot(LSHDev.sample1["description"],LSHDev.sample2["description"],LSHDev.sample3["description"])
         #print("It should be safe to exit for the next 2 seconds: CTRL-C to exit")
         #sleep(2)
